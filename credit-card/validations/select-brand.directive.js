f6b18a225c0f38a4166adecc5dbf70ecc6775b65
Denilson Rodrigues <denilson.santos@mundipagg.com>
2017-10-26 16:09:03 -0200

    :bug: Retira arrow functions

--- credit-card/validations/select-brand.directive.js
+++ credit-card/validations/select-brand.directive.js
@@ -5,10 +5,10 @@ export default function cardSelectBrand($window, _Format, Cards, $parse) {
         require: 'ngModel',
         link: function (scope, elem, attr, ngModel) {
             var oldBrand = '';
-            elem[0].addEventListener('focus',(event)=>{ 
+            elem[0].addEventListener('focus',function (event){ 
                 oldBrand = ngModel.$modelValue;
             });
-            elem[0].addEventListener('change',(event)=>{
+            elem[0].addEventListener('change',function (event){
                 if(oldBrand !== ngModel.$modelValue){
                     Cards.setWarningState(scope,attr,'manualMode');
                     oldBrand = ngModel.$modelValue;
--- test/test.module.js
+++ test/test.module.js
@@ -2,7 +2,7 @@ import angular from 'angular';
 import angular_br_credit_card from '../credit-card/credit-card.module';
 
 angular.module("myShoppingList", [angular_br_credit_card]).controller("myCtrl", function ($scope) {
-    setInterval(()=>{
+    setInterval(function (){
         console.log(this);
     },1000);
 });
