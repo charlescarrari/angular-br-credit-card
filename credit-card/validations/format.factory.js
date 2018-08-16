6a63dce91dc7502da38bb022b4330864d57c696b
Charles Carrari <ccarrari@mundipagg.com>
2018-08-16 20:52:26 -0300

    Ajustes de eventos

--- credit-card/validations/format.factory.js
+++ credit-card/validations/format.factory.js
@@ -308,6 +308,12 @@ export function Format(Cards, Common, $filter) {
         elem.bind('keypress', _formatExpiry);
         elem.bind('keypress', _formatForwardSlash);
         elem.bind('keypress', _formatForwardExpiry);
+
+        elem.bind('keydown', _restrictExpiry);
+        elem.bind('keydown', _formatExpiry);
+        elem.bind('keydown', _formatForwardSlash);
+        elem.bind('keydown', _formatForwardExpiry);
+
         elem.bind('keydown', _formatBackExpiry);
 
         ctrl.$parsers.push(_parseExpiry);
