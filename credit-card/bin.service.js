fd4021541162365d943125ffa21f0e98cebecbe7
Pedro Gustavo <eu@pedrogustavo.com>
2020-04-13 17:20:28 -0300

    :art: Implementa nova api de bins

--- credit-card/bin.service.js
+++ credit-card/bin.service.js
@@ -5,7 +5,7 @@ binService.$inject = ['$q', '$http'];
 /////////
 
 export default function binService($q, $http) {
-	var api = 'https://api.mundipagg.com/bin/v1';
+	var api = 'https://api.mundipagg.com';
 	var service = {
 		getBrandsByBIN: _getBrandsByBIN
 	};
@@ -16,10 +16,12 @@ export default function binService($q, $http) {
 
 	function _getBrandsByBIN(bin) {
 		var deferred = $q.defer();
+		var body = JSON.stringify({ 'query': 'query UtilitiesQuery { brands (bin:\"' + bin + '\") {key, name, gaps, lenghts, mask, cvv, image, bins } }'})
 
-		$http.get(api + '/' + bin)
+		$http.post(api + '/utilities/v2/graphql', body)
 			.then(function (response) {
-				deferred.resolve(response);
+				if (response.data.brands.length) deferred.resolve(response.data.brands[0]);
+				else deferred.resolve(response.data.brands);
 			})
 			.catch(function (response) {
 				deferred.reject(response);
